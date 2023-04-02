110dcbb1a945fcae872e7170c9d58104295c826b
Denis Galper <denis_1293@hotmail.com>
2023-04-02 14:26:13 +0300

    calc not finished

---
+++ src/telran/numbers/test/CalculatorTest.java
@@ -0,0 +1,34 @@
+package telran.numbers.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSubtract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDivide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemainder() {
+		fail("Not yet implemented");
+	}
+
+}
